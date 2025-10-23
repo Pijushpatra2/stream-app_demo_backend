332ebe6965233b9583524de9eb93fab08c00f370
Pijush Patra <94787140+Pijushpatra2@users.noreply.github.com>
2025-10-23 16:27:06 +0530

    Add files via upload
    1.2

---
+++ server.js
@@ -0,0 +1,58 @@
+// server.js
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import sequelize from './config/db.js';
+import morgan from 'morgan';
+
+import S_A_Router from "./routes/superAdminRoutes.js";
+import videoRoutes from './routes/video/videoRoutes.js';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Configure CORS properly
+const corsOptions = {
+  origin: [
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+//Test DB connection
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected successfully from server side');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error.message);
+    process.exit(1);
+  }
+})();
+
+//Test route
+app.get('/', (req, res) => {
+  res.send('Server is running with MySQL connection!');
+});
+
+app.use('/api/v1/superAdmin', (S_A_Router))
+// Routes
+app.use('/api/v1/videos', videoRoutes);
+
+
+//Start Server
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
